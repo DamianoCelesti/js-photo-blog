c5e5ef04847d2264cb206fceb433a2eec556d77c
DamianoCelesti <damiano.celesti@gmail.com>
2025-01-27 15:25:18 +0100

    add event listener

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Seleziono tutti gli elementi delle card
-const cards = document.querySelectorAll('.col');
-console.log(cards);
+const cards = document.querySelectorAll('.card');
 
 // Endpoint dell'API
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
@@ -10,30 +9,40 @@ const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
 
 axios.get(endpoint)
     .then((response) => {
-        console.log(response);
+        //console.log(response);
         const data = response.data;
-        console.log(data);
+        //console.log(data);
         
-        // for (img of data) {
-        //     console.log(img.url);
-        //     const imgVacanze = img.url
-        //     cards.innerHTML += `<img src="${imgVacanze}" class="card-img-top p-3" alt="img 1">`;
-        // }
         let index = 0;
-        for(let i = 0; i < data.length; i++){
+        for(let i = 0; i < data.length; i++) {
             const infoViaggi = data[i];
-            //console.log(infoViaggi.url);
+            //console.log(infoViaggi);
             
-            cards[index].innerHTML += `<div class="card">
+            cards[index].innerHTML += `
             <img src="./img/pin.svg" class="pin" alt="pin">
             <img src="${infoViaggi.url}" class="card-img-top p-3" alt="img ${index +1}">
             <div class="card-body">
                 <p class="card-text mb-0 ">${infoViaggi.date}</p>
                 <h2 class="card-title text-uppercase fw-bold">${infoViaggi.title}</h2> 
-            </div>
             </div>`;
+            //console.log('123', cards[index]);
+            
+
+            
             index++;
         } 
         
         
-    })
+        
+
+});
+
+
+const hidden = document.querySelector('.hidden');
+
+for (let i = 0; i < cards.length; i++) {
+    let currentCard = cards[i];
+    currentCard.addEventListener('click', () => {
+        hidden.classList.remove('hidden');
+    });
+}
